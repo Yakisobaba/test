014745a27b1b6c4554a9c7ef289fe37f3ae46c1a
Yakisobaba <86834669+Yakisobaba@users.noreply.github.com>
2024-01-14 15:37:07 +0900

    armMove 1
    0.1秒で90度動かす

--- Assets/player/PlayerArm.cs
+++ Assets/player/PlayerArm.cs
@@ -6,30 +6,42 @@ public class PlayerArm : MonoBehaviour
 {
     [SerializeField]
     bool right;
+
+    float armSpead=8;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-        if(right) 
+
+        if (right)
         {
-            if (Input.GetMouseButton(1))
+            if (Input.GetMouseButtonDown(1)&&armSpead>=0)
             {
-                transform.eulerAngles += new Vector3(0,-10f,0);
+                StartCoroutine(Rotate(-1));
             }
         }
         else
         {
-            if (Input.GetMouseButton(0))
+            if (Input.GetMouseButtonDown(0) && armSpead >= 0)
             {
-                transform.eulerAngles += new Vector3(0, 10f, 0);
+                StartCoroutine(Rotate(1));
             }
         }
-        
+
+    }
+
+    IEnumerator Rotate(int a)
+    {
+        for (int i = 0; i < 30; i++) 
+        {
+            transform.eulerAngles += new Vector3(0, a*3, 0);
+            yield return new WaitForSeconds(0.13f/30);
+        }
+
     }
 }
